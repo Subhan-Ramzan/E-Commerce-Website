b0f7ad86fdbc63e06119b1872b3a7923126da043
Subhan <subhanramzan215@gmail.com>
2024-11-07 00:14:43 +0500

    fileChanges

--- app/page.js
+++ app/page.js
@@ -1,11 +1,9 @@
-import Image from "next/image";
 import Main from "@/components/Main";
 import Category from "@/components/Category";
 import Coverimg from "@/components/CoverImg";
 import HeroSection from "@/components/HeroSection";
 import FeaturedProducts from "@/components/FeaturedProducts";
 import CategorySection from "@/components/CategorySection";
-// import UploadImage from "@/components/UploadImage";
 
 export default function Home() {
   return (
@@ -16,7 +14,6 @@ export default function Home() {
       <CategorySection />
       <Coverimg />
       <Main />
-      {/* <UploadImage /> */}
     </>
   );
 }
--- components/UploadImage.jsx
+++ components/UploadImage.jsx
@@ -1,29 +1,29 @@
-'use client'; // Ensure this is at the top for client-side rendering
+// 'use client'; // Ensure this is at the top for client-side rendering
 
-import { CldImage, CldUploadWidget } from 'next-cloudinary';
-import { useState } from 'react';
-export default function UploadImage() {
-    const [publicid, setpublicId] = useState("");
-    return (
-        <>
-        {publicid && <CldImage src={publicid} alt={publicid} width={300} height={200} />}
-            <CldUploadWidget uploadPreset="next-image" onSuccess={({ event, info }) => {
-                if (event === "success") {
-                    setpublicId(info?.public_id)
-                }
-            }}>
-                {({ open }) => {
-                    return (
-                        <button
-                            className="bg-green-600 text-white rounded-xl p-4 hover:bg-green-400 transform transition-all duration-300 ease-in-out shadow-lg hover:scale-105 hover:shadow-xl"
-                            onClick={() => open()}
-                        >
-                            Upload an Image
-                        </button>
+// import { CldImage, CldUploadWidget } from 'next-cloudinary';
+// import { useState } from 'react';
+// export default function UploadImage() {
+//     const [publicid, setpublicId] = useState("");
+//     return (
+//         <>
+//         {publicid && <CldImage src={publicid} alt={publicid} width={300} height={200} />}
+//             <CldUploadWidget uploadPreset="next-image" onSuccess={({ event, info }) => {
+//                 if (event === "success") {
+//                     setpublicId(info?.public_id)
+//                 }
+//             }}>
+//                 {({ open }) => {
+//                     return (
+//                         <button
+//                             className="bg-green-600 text-white rounded-xl p-4 hover:bg-green-400 transform transition-all duration-300 ease-in-out shadow-lg hover:scale-105 hover:shadow-xl"
+//                             onClick={() => open()}
+//                         >
+//                             Upload an Image
+//                         </button>
 
-                    );
-                }}
-            </CldUploadWidget>
-        </>
-    );
-}
+//                     );
+//                 }}
+//             </CldUploadWidget>
+//         </>
+//     );
+// }
