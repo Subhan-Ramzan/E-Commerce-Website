a433fa0cf853e4b9d9986e96071ed08625cd3268
Subhan <subhanramzan215@gmail.com>
2025-02-04 21:21:55 +0500

    NextConfigUrl

--- next.config.mjs
+++ next.config.mjs
@@ -4,13 +4,12 @@ const nextConfig = {
     domains: [
       "localhost",
       "127.0.0.1",
-      "your-strapi-domain.com",
+      "giving-poem-6ff11e31eb.media.strapiapp.com",
       "lh3.googleusercontent.com",
       "res.cloudinary.com",
       "via.placeholder.com",
       "avatars.githubusercontent.com",
     ],
-
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
     imageSizes: [16, 32, 48, 64, 96],
     path: "/_next/image",
